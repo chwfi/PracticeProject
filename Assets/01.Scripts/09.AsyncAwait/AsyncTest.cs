1d1b91ae56ca919091573a3e3e3cc545dc590834
Chwfi <98933635+chwfi@users.noreply.github.com>
2023-10-31 14:28:48 +0900

    async와await
    async와await

---
+++ Assets/01.Scripts/09.AsyncAwait/AsyncTest.cs
@@ -0,0 +1,18 @@
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class AsyncTest : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Start!!!");
+        Test();
+        Debug.Log("1");
+    }
+    private async void Test()
+    {
+        Debug.Log("2");
+        await Task.Delay(7000);
+        Debug.Log("3");
+    }
+}
